3950f09ec976213f848a1f548dcda8d42d439af1
ALX99 <46844683+ALX99@users.noreply.github.com>
2022-12-23 20:27:58 +0100

    feat: create grafana db user

--- db/init.sql
+++ db/init.sql
@@ -1,3 +1,13 @@
+CREATE ROLE readaccess;
+
+GRANT USAGE ON SCHEMA public TO readaccess;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO readaccess;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO readaccess;
+
+CREATE USER grafana WITH PASSWORD 'example';
+GRANT readaccess TO grafana;
+
 CREATE TABLE IP (
     ip_address inet NOT NULL,
     PRIMARY KEY (ip_address)
