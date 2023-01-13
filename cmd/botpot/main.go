4b75dedf33a22502d7083794894023a97170c53e
ALX99 <46844683+ALX99@users.noreply.github.com>
2023-01-13 18:03:14 +0100

    refactor: use SIGINT

--- cmd/botpot/main.go
+++ cmd/botpot/main.go
@@ -72,7 +72,7 @@ func main() {
 	}
 
 	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 	<-c
 
 	err = sshServer.Stop()
